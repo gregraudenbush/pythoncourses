833a213b8d6d2e1ca2999a10737caa0af53912c5
Gregory Raudenbush <gregraudenbush@gmail.com>
2017-12-18 16:59:07 -0800

    re deploy test

--- products/settings.py
+++ products/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '00#o^3kvuh&5^u-s$$+ftqxcij&scz3f*_m0(wu_1+8jf(3+5o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.91.217.80']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
